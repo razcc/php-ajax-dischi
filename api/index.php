43aca62e83304f1b04be2e41ebb24d6cf18ca98c
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-12-15 20:30:46 +0100

    Struttura file, html css di base, db, api file

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<!-- API -->
+<?php
+include_once __DIR__ . '/../dataBase/index.php';
+
+?>
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+
+    },
+    mounted() {
+
+    },
+    beforeUpdate() {
+
+    },
+    methods: {
+
+    }
+})
