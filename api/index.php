427404a8b2d3aa68d9836bd82f26b938ab3404fe
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-12-16 08:30:20 +0100

    Chiamata axios, e stampa a shcemro di tutti i disc

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
-<!-- API -->
 <?php
-include_once __DIR__ . '/../dataBase/index.php';
+include __DIR__ . '/../db/index.php';
+
+
+// var_dump($database);
+header('Content-type: application/json');
+
+echo json_encode($database);
 
 ?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 var app = new Vue({
     el: '#app',
     data: {
+        arrayDischi: [],
 
     },
     mounted() {
+        axios.get('http://localhost/php-ajax-dischi/api/')
+            .then((response) => {
 
+                this.arrayDischi = response.data
+                console.log(this.arrayDischi)
+
+            })
     },
     beforeUpdate() {
 
